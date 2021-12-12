a70314b655fb08e2fe7e4883351b8c25038e4cb5
Felipe Souza <felipedb91@gmail.com>
2021-12-11 22:07:54 -0300

    feat: finish project

--- lib/componetns/store_tile.dart
+++ lib/componetns/store_tile.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class StoreTile extends StatelessWidget {
   const StoreTile({Key? key, required this.data}) : super(key: key);
@@ -9,18 +10,17 @@ class StoreTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           SizedBox(
             height: 150,
-            width: double.infinity,
             child: Image.network(
               data.data()!["imageUrl"],
               fit: BoxFit.cover,
               alignment: Alignment.center,
             ),
           ),
-          SizedBox(height: 15),
+          SizedBox(height: 12),
           Container(
             margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
             child: Column(
@@ -41,8 +41,17 @@ class StoreTile extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
-              TextButton(onPressed: () {}, child: Text("Ver no mapa")),
-              TextButton(onPressed: () {}, child: Text("Ligar")),
+              TextButton(
+                  onPressed: () {
+                    launch(
+                        "https://www.google.com/maps/search/?api=1&query=${data.data()!["lat"]},${data.data()!["long"]}");
+                  },
+                  child: Text("Ver no mapa")),
+              TextButton(
+                  onPressed: () {
+                    launch("tel:${data.data()!["phone"]}");
+                  },
+                  child: Text("Ligar")),
             ],
           )
         ],
